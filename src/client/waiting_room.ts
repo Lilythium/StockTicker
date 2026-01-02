863dac9645c7ccde70e86290a822447008e61063
Monika Trenholme <monika-trenholme@protonmail.com>
2026-01-02 12:17:06 -0400

    add copy button indicator

--- src/client/waiting_room.ts
+++ src/client/waiting_room.ts
@@ -18,7 +18,13 @@ game_link.value = `${ORIGIN}/?game_id=${GAME_ID}`;
 
 copy_button.addEventListener("click", () => {
     navigator.clipboard.writeText(game_link.value).then(() => {
-        // TODO: put copy indicator here
+        copy_button.textContent = 'Copied!';
+        copy_button.style.background = '#27ae60';
+        
+        setTimeout(() => {
+            copy_button.textContent = 'Copy';
+            copy_button.style.background = '';
+        }, 2000);
     });
 });
 
